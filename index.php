d2ee10b4e057be04b1e6cc8b60b8e644fc79037d
Justin Scott <fligtar@gmail.com>
2011-02-20 23:40:55 -0800

    initial design

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Add-ons</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+    <header>
+        <h1><img src="images/mosaic.png" alt=""/>Add-ons</h1>
+    </header>
+    
+    <section id="content">
+        <section>
+            <h2>Platform</h2>
+            <div>
+                <p>hi</p>
+            </div>
+        </section>
+        
+        <section>
+            <h2>Performance</h2>
+            <div>
+                <p>hi</p>
+            </div>
+        </section>
+        
+        <section>
+            <h2>Penetration</h2>
+            <div>
+                <p>hi</p>
+            </div>
+        </section>
+    </section>
+</body>
+</html>
